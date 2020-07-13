b1fecdbddfc2adc9940475e33f115076d83f2ff3
cmsebastian <chris.sebastian3@gmail.com>
2020-07-13 07:50:45 -0700

    New layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
+const homePage = document.querySelector('#home');
+const portfolioPage = document.querySelector('#portfolio');
+const contactPage = document.querySelector('#contact');
+
+// document.getElementById("#home").addEventListener("click", homeLoad);
+// document.getElementById("#portfolio").addEventListener("click", portLoad);
+// document.getElementById("#contact").addEventListener("click", contactLoad);
+
+// window.onload = function () {
+//     document.getElementById("home").style.display = "block";
+//     document.querySelector('#portfolio').style.display = "none";
+//     document.querySelector('#contact').style.display = "none";
+// };
+
+// function homeLoad() {
+//     document.getElementById("home").style.display = "block";
+//     document.querySelector('#portfolio').style.display = "none";
+//     document.querySelector('#contact').style.display = "none";
+// };
+
+// function portLoad() {
+//     document.getElementById("#portfolio").style.display = "block";
+//     document.querySelector('#home').style.display = "none";
+//     document.querySelector('#contact').style.display = "none";
+// };
+
+// function contactLoad() {
+//     document.getElementById("#contact").style.display = "block";
+//     document.querySelector('#portfolio').style.display = "none";
+//     document.querySelector('home').style.display = "none";
+// }
+
+
+
 $('#myCarousel').carousel();
